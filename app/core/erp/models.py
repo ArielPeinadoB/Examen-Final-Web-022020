b7dda173495c9f446a5627558d65cdfb96fbed46
Ariel Peinado <apeinadob@miumg.edu.gt>
2020-11-15 20:10:11 -0600

    CRUD DE EVUALUACION FINAL

---
+++ app/core/erp/models.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+
+from django.db import models
+from django.forms import model_to_dict
+
+from config.settings import MEDIA_URL, STATIC_URL
+from core.erp.choices import gender_choices
+from core.models import BaseModel
+
+
+class Categoria(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Nombre', unique=True)
+    desc = models.CharField(max_length=500, null=True, blank=True, verbose_name='Descripción')
+
+    def __str__(self):
+        return self.name
+
+    def toJSON(self):
+        item = model_to_dict(self)
+        return item
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['id']
+
+
+class Equipos(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Nombre', unique=True)
+    cat = models.ForeignKey(Categoria, on_delete=models.CASCADE, verbose_name='Categoría')
+    image = models.ImageField(upload_to='product/%Y/%m/%d', null=True, blank=True, verbose_name='Imagen')
+    pvp = models.DecimalField(default=0.00, max_digits=9, decimal_places=2, verbose_name='Precio de venta')
+
+    def __str__(self):
+        return self.name
+
+    def toJSON(self):
+        item = model_to_dict(self)
+        item['cat'] = self.cat.toJSON()
+        item['image'] = self.get_image()
+        item['pvp'] = format(self.pvp, '.2f')
+        return item
+
+    def get_image(self):
+        if self.image:
+            return '{}{}'.format(MEDIA_URL, self.image)
+        return '{}{}'.format(STATIC_URL, 'img/empty.png')
+
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+        ordering = ['id']
+
+
